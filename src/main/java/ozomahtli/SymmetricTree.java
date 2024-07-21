3461039b06e29a6a286b625c92a9ca9d8c12474e
Your Name <diegod9405@gmail.com>
2024-07-20 19:54:22 -0600

    adding main class

---
+++ src/main/java/ozomahtli/SymmetricTree.java
@@ -0,0 +1,18 @@
+package ozomahtli;
+
+import utils.TreeNode;
+
+public class SymmetricTree {
+
+        public boolean isSymmetric(TreeNode root) {
+            return isSymmetric(root, root);
+        }
+
+        private boolean isSymmetric(TreeNode p, TreeNode q) {
+            if (p == null || q == null)
+                return p == q;
+
+            return p.val == q.val && isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);
+        }
+
+}
