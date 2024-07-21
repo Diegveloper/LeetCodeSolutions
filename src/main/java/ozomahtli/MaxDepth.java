0ff0fe04eafdf329a951390fef31daea86aefed1
Your Name <diegod9405@gmail.com>
2024-07-20 20:25:48 -0600

    adding main class

---
+++ src/main/java/ozomahtli/MaxDepth.java
@@ -0,0 +1,11 @@
+package ozomahtli;
+
+import utils.TreeNode;
+
+public class MaxDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
