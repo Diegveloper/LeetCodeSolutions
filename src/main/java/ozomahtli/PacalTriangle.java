6d600ee474d130f09dbd9fec19b18db8e65fe583
Your Name <diegod9405@gmail.com>
2024-07-29 12:38:27 -0600

    adding main class

--- src/main/java/ozomahtli/PacalTriangle.java
+++ src/main/java/ozomahtli/PacalTriangle.java
@@ -20,4 +20,21 @@ public class PacalTriangle {
 
         return ans;
     }
+    public List<Integer> getRow2(int r) {
+        int numRows = r+1;
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < numRows; ++i) {
+            Integer[] temp = new Integer[i + 1];
+            Arrays.fill(temp, 1);
+            ans.add(Arrays.asList(temp));
+        }
+
+        for (int i = 2; i < numRows; ++i)
+            for (int j = 1; j < ans.get(i).size() - 1; ++j)
+                ans.get(i).set(j, ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
+
+        return ans.get(r);
+    }
+
 }
