0d53aad6de38e5d7e7a784b5a009e2467a041be4
Your Name <diegod9405@gmail.com>
2024-08-05 16:09:51 -0600

    adding main class

---
+++ src/main/java/ozomahtli/LinkedListCycle.java
@@ -0,0 +1,38 @@
+package ozomahtli;
+
+import java.util.HashMap;
+
+
+public class LinkedListCycle {
+    public boolean hasCycle1(ListNode head) {
+        ListNode t = head;
+        int c = 0;
+        HashMap<ListNode, String> hm = new HashMap<>();
+        if(head == null){return false;}
+        while(t.next != null){
+            t = t.next;
+            if(!hm.containsKey(t)){
+                hm.put(t,"");
+            }
+            else if(hm.containsKey(t)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+}
